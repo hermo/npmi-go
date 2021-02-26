ac6c79833e9355ee17d1d5c75b26db278c77e004
Mirko Grönroos <mirko@mirko.fi>
2021-02-26 14:42:26 +0200

    Implement NPM installation

--- cmd/npmi/main.go
+++ cmd/npmi/main.go
@@ -7,78 +7,148 @@ import (
 
 	"github.com/hermo/npmi-go/pkg/archive"
 	"github.com/hermo/npmi-go/pkg/cache"
+	"github.com/hermo/npmi-go/pkg/files"
 	"github.com/hermo/npmi-go/pkg/npmi"
 )
 
-func main() {
-	options, _ := ParseFlags()
-	fmt.Printf("Flags: %+v\n", options)
-	env, err := npmi.DeterminePlatform()
-	if err != nil {
-		log.Fatalf("Can't determine Node.js version: %v", err)
-	}
-	fmt.Printf("Node env: %s\n", env)
+const (
+	modulesDirectory = "node_modules"
+	lockFile         = "package-lock.json"
+)
 
-	hash, err := npmi.HashFile("package-lock.json")
+func init() {
+	if err := npmi.InitNodeBinaries(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	options, err := ParseFlags()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Hash: %s\n", hash)
-
-	key := createKey(env, hash)
+	env, err := npmi.DeterminePlatform()
+	if err != nil {
+		log.Fatalf("Can't determine Node.js version: %v", err)
+	}
 
 	caches, err := initCaches(options)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("-- Lookup phase --")
+	hash, err := npmi.HashFile(lockFile)
+	if err != nil {
+		log.Fatalf("Can't hash %s: %s", lockFile, err)
+	}
+
+	key := createKey(env, hash)
+
+	if options.Verbose {
+		fmt.Printf("Key: %s\n", key)
+		fmt.Println("Lookup start")
+	}
+
 	hit := false
 	for _, cache := range caches {
-		hit, err := cache.Has(key)
+		hit, err = cache.Has(key)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Lookup cache(%T).Has error: %s", cache, err)
 		}
 		if hit {
-			fmt.Printf("%T HIT, extracting\n", cache)
+			if options.Verbose {
+				fmt.Printf("Lookup cache(%T) HIT, extracting\n", cache)
+			}
 			f, err := cache.Get(key)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Lookup cache(%T).Get error: %s", cache, err)
 			}
-			err = archive.ExtractArchive(f)
+			err = archive.ExtractArchive("", f)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Lookup cache(%T).Extract error: %s", cache, err)
 			}
 			break
 		}
 	}
 
-	fmt.Println("-- Cache phase --")
-	filename := fmt.Sprintf("%s.tar.gz", key)
-	err = archive.Archive(filename, "pkg")
-	if err != nil {
-		log.Fatal(err)
+	if options.Verbose {
+		fmt.Println("Lookup phase complete")
 	}
 
-	for _, cache := range caches {
-		if options.Force || !hit {
-			fmt.Printf("%T MISS, updating cache\n", cache)
+	if options.Force || !hit {
+		if options.Verbose {
+			fmt.Println("Install phase start")
+			if options.Force && hit {
+				fmt.Println("NOTE: Cache was a HIT, install is forced")
+			}
+		}
+
+		stdout, stderr, err := npmi.InstallPackages()
+		if err != nil {
+			log.Fatalf("Install NPM error: %v: %s", err, stderr)
+		}
+		if options.Verbose {
+			fmt.Printf("Install NPM success: %s\n", stdout)
+		}
+		if !files.IsExistingDir(modulesDirectory) {
+			log.Fatalf("Post-install: Modules directory not present after NPM install: %s", modulesDirectory)
+		}
+		if options.Verbose {
+			fmt.Println("Install phase complete")
+		}
+		filename := fmt.Sprintf("modules-%s.tar.gz", key)
+		if options.Verbose {
+			fmt.Println("Archive phase start")
+			fmt.Printf("Archive creating %s\n", filename)
+		}
+		err = archive.Archive(filename, modulesDirectory)
+		if err != nil {
+			log.Fatalf("Archive failed: %s", err)
+		}
+
+		// Remove temp file when done
+		defer func() {
+			err := os.Remove(filename)
+			if err != nil {
+				log.Fatalf("Could not remove temporary archive: %v", err)
+			}
+			if options.Verbose {
+				fmt.Printf("Removed temporary archive %s", filename)
+			}
+		}()
+
+		if options.Verbose {
+			fmt.Println("Archive phase complete")
+			fmt.Println("Cache phase start")
+		}
+
+		for _, cache := range caches {
+			if options.Verbose {
+				fmt.Printf("Cache(%T) Open temporary archive\n", cache)
+			}
 
 			f, err := os.Open(filename)
 			defer f.Close()
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Cache(%T) Open error: %s", cache, err)
 			}
 
+			if options.Verbose {
+				fmt.Printf("Cache(%T) Put start\n", cache)
+			}
 			err = cache.Put(key, f)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatalf("Cache(%T) Put error: %s", cache, err)
+			}
+			if options.Verbose {
+				fmt.Printf("Cache(%T) Put complete\n", cache)
 			}
 		}
-
 	}
-
+	if options.Verbose {
+		fmt.Println("Cache phase complete")
+	}
 }
 
 func createKey(env string, hash string) string {
--- pkg/npmi/npmi.go
+++ pkg/npmi/npmi.go
@@ -5,26 +5,44 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+var (
+	nodeBinary string
+	npmBinary  string
+)
+
 // DeterminePlatform determines the node platform
 func DeterminePlatform() (string, error) {
-	nodeBinary, err := exec.LookPath("node")
-	if err != nil {
-		log.Fatal("Can't find a node binary in path")
+	if nodeBinary == "" || npmBinary == "" {
+		return "", fmt.Errorf("DeterminePlatform: InitNodeBinaries not run")
 	}
 
 	cmd := exec.Command(nodeBinary, "-p", `process.version + "-" + process.platform + "-" + process.arch`)
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	if err = cmd.Run(); err != nil {
+	if err := cmd.Run(); err != nil {
 		return "", err
 	}
 	return strings.TrimSpace(out.String()), nil
+}
+
+// InitNodeBinaries makes sure that required Node.JS binaries are present
+func InitNodeBinaries() error {
+	var err error
+	nodeBinary, err = exec.LookPath("node")
+	if err != nil {
+		return err
+	}
+
+	npmBinary, err = exec.LookPath("npm")
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // HashFile attempts to hash a file using SHA-256
@@ -45,4 +63,17 @@ func hashHandle(handle io.Reader) (string, error) {
 	}
 
 	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+// InstallPackages installs packages from NPM
+func InstallPackages() (stdout string, stderr string, err error) {
+	cmd := exec.Command(npmBinary, "ci")
+	var stdoutBuf bytes.Buffer
+	var stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+	err = cmd.Run()
+	stdout = strings.TrimSpace(stdoutBuf.String())
+	stderr = strings.TrimSpace(stderrBuf.String())
+	return
 }
