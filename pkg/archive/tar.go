3b125feec9f8fa318318c765e03cdadf4835219f
Mirko Grönroos <mirko@mirko.fi>
2021-02-26 14:42:01 +0200

    Implement untar

--- pkg/archive/tar.go
+++ pkg/archive/tar.go
@@ -37,8 +37,8 @@ func Archive(filename string, src string) error {
 }
 
 // ExtractArchive uncompresses stuff
-func ExtractArchive(reader io.Reader) error {
-	return nil
+func ExtractArchive(dst string, reader io.Reader) error {
+	return untar(dst, reader)
 }
 
 // createTarGz writes a given directory tree to a Gzipped TAR
@@ -112,4 +112,70 @@ func createTarGz(src string, writers ...io.Writer) error {
 
 		return nil
 	})
+}
+
+// untar takes a destination path and a reader; a tar reader loops over the tarfile
+// creating the file structure at 'dst' along the way, and writing any files
+func untar(dst string, r io.Reader) error {
+
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		// if the header is nil, just skip it (not sure how this happens)
+		if header == nil {
+			fmt.Println("Nil header?")
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
+	return nil
 }
