6b11c763277c006cd33b07cb2542da7ee325c767
Mirko Grönroos <mirko@mirko.fi>
2021-04-08 21:12:15 +0300

    Add test for HashString

--- pkg/npmi/npmi_test.go
+++ pkg/npmi/npmi_test.go
@@ -164,3 +164,27 @@ func TestInitNodeBinaries(t *testing.T) {
 		})
 	}
 }
+
+func TestHashString(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    string
+		wantErr bool
+	}{
+		{"Empty string", "", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", false},
+		{"Hello string", "hello", "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := HashString(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HashString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("HashString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
