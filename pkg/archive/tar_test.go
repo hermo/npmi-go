c5bdc3b2b10eda0103511b4017c052eb1abadfa4
Mirko Grönroos <mirko@verkkokauppa.com>
2021-08-20 10:50:41 +0300

    Add failing test for evil tar archives

--- pkg/archive/tar_test.go
+++ pkg/archive/tar_test.go
@@ -4,12 +4,23 @@ import (
 	"archive/tar"
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"os"
+	"path"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/hermo/npmi-go/pkg/files"
 )
+
+func getBaseDir() string {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("No caller information")
+	}
+	return path.Dir(filename)
+}
 
 func Test_ExtractFilesNormal(t *testing.T) {
 	var buf bytes.Buffer
@@ -44,18 +55,18 @@ func Test_ExtractFilesNormal(t *testing.T) {
 	tw.Close()
 	gzw.Close()
 
-	testDataDir, err := filepath.Abs("../../testdata")
+	testBaseDir, err := filepath.Abs(fmt.Sprintf("%s/../../testdata", getBaseDir()))
 	if err != nil {
 		t.Fatalf("Can't find test directory: %v", err)
 	}
 
-	testDir, err := os.MkdirTemp(testDataDir, "test")
+	testDir, err := os.MkdirTemp(testBaseDir, "test")
 	if err != nil {
 		t.Fatalf("Can't create temporary test directory: %v", err)
 	}
 
 	defer func() {
-		os.Chdir("../..")
+		os.Chdir(getBaseDir())
 		err := os.RemoveAll(testDir)
 		if err != nil {
 			t.Fatalf("Could not remove temp directory: %v", err)
@@ -97,6 +108,77 @@ func Test_ExtractFilesNormal(t *testing.T) {
 		}
 		if !exists {
 			t.Fatalf("File %s should exists but does not", f.Name)
+		}
+	}
+}
+
+func Test_ExtractFilesEvil(t *testing.T) {
+	evilPayloads := []struct {
+		Name    string
+		Content string
+		Type    byte
+	}{
+		{"../evil.txt", "evil", tar.TypeReg},
+		{"./../evil.txt", "evil", tar.TypeReg},
+	}
+
+	testBaseDir, err := filepath.Abs(fmt.Sprintf("%s/../../testdata", getBaseDir()))
+	if err != nil {
+		t.Fatalf("Can't find test data directory: %v", err)
+	}
+
+	for _, f := range evilPayloads {
+		testDir, err := os.MkdirTemp(testBaseDir, "test")
+		if err != nil {
+			t.Fatalf("Can't create temporary test directory: %v", err)
+		}
+
+		defer func() {
+			os.Chdir(getBaseDir())
+			err := os.RemoveAll(testDir)
+			if err != nil {
+				t.Fatalf("Could not remove temp directory: %v", err)
+			}
+		}()
+
+		err = os.Chdir(testDir)
+		if err != nil {
+			t.Fatalf("Can't chdir to test directory: %v", err)
+		}
+
+		err = os.Mkdir("extract", 0700)
+		if err != nil {
+			t.Fatalf("Could not create directory: %v", err)
+		}
+
+		err = os.Chdir("extract")
+		if err != nil {
+			t.Fatalf("Can't chdir to test directory: %v", err)
+		}
+
+		var buf bytes.Buffer
+		gzw := gzip.NewWriter(&buf)
+		tw := tar.NewWriter(gzw)
+
+		data := []byte(f.Content)
+		hdr := tar.Header{
+			Typeflag: f.Type,
+			Name:     f.Name,
+		}
+		if f.Type == tar.TypeReg {
+			hdr.Size = int64(len(data))
+		}
+		tw.WriteHeader(&hdr)
+		if f.Type == tar.TypeReg {
+			tw.Write(data)
+		}
+
+		tw.Close()
+		gzw.Close()
+
+		_, err = Extract(&buf)
+		if err == nil {
+			t.Fatalf("Extract should have failed but did not")
 		}
 	}
 }
